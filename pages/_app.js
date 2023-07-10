bd67312efbc6b5af2f4c9edd61caa714ed2c6879
Manuel Rivera <menu.rivera@gmail.com>
2023-07-09 18:43:02 -0600

    [ADD] -> layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
+import Layout from '@/components/layout'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<Layout>
+			<Component {...pageProps} />
+		</Layout>
+	)
 }
